1aa82f579274c80d47724c75d78f488d4c9a5dee
errorreplicator <error.replicator@gmail.com>
2016-11-19 14:49:05 +0100

    changes to reg_ex

--- LRegresion.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as mlp
-
-x = [1,2,3,4,5,6]
-y = [5,4,6,5,6,7]
-
-mlp.scatter(x,y)
-mlp.ylim(0,10)
-mlp.xlim(0,10)
-mlp.show()
--- pandas_columns.py
+++
@@ -1,8 +0,0 @@
-ds.rename(columns={'col1':'First','col2':'2nd','col3':'3rd','Unnamed: 4':'4th'}, inplace=True)
-ds.drop('Unnamed: 0',1,inplace=True)
-print(ds.loc[1:4,'2nd':])
-ds = pd.read_csv('census.dat',names=['education', 'age', 'capital-gain', 'race', 'capital-loss', 'hours-per-week', 'sex', 'classification'])
-print(ds['capital-gain'][ds['capital-gain'] == '?'])
-
-
-ds = pd.DataFrame(np.random.choice([0.9,4.1,'2ta',np.nan], size=(10,5)),columns=['fir','sec','thir','4th','5th'])
